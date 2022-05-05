4e0e8f8118dc04403621b761601a51beef95332f
Mykhailo Hrytskov <mykhailo.hrytskov@dish.com>
2022-05-05 22:08:02 +0300

    Added react-navigation and Routes type

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+
+type Routes = {
+  Home: string;
+};
+
+const routes: Routes = {
+  Home: 'Home',
+};
+
+const Home = () => (
+  <View style={styles.container}>
+    <Text>Open up App.tsx to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>);
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={routes.Home}>
+        <Stack.Screen name={routes.Home} component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
